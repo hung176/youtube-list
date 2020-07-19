6e678a94d529c85fb8eea8ec3332cc5853df61a5
hungnguyen <nvhung.bme@gmail.com>
2020-07-19 21:28:50 +0700

    Search feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    result: { 
+      items: []
+    },
+    loading: false,
+    searchQuery: ''
+  }
+
+  componentDidUpdate(_, previousState) {
+    if (previousState.searchQuery !== this.state.searchQuery) {
+      this.setState({ loading: true });
+      fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.searchQuery}&type=video&key=${process.env.REACT_APP_YOUTUBE_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ result: data, loading: false });
+        });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.loading && (
+          <div>Loading ... </div>
+        )}
+        <input onChange={(ev) => this.setState({ searchQuery: ev.target.value })} value={this.state.searchQuery} />
+        {this.state.searchQuery}
+        {this.state.result.items.map(item => (
+          <a key={item.id.videoId} href={`https://www.youtube.com/watch?v=${item.id.videoId}`} target="_blank">
+            <div>
+              <div>{item.snippet.title}</div>
+              <img src={item.snippet.thumbnails.high.url} />                
+            </div>
+          </a>
+        ))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
