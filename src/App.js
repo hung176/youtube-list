c484ecfc5138b93c037bf76da915bf23f5d5312c
hungnguyen <nvhung.bme@gmail.com>
2020-07-31 10:59:14 +0700

    style searchbar by autocomplete antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
+import { SearchOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
 import './App.css';
+import { AutoComplete, Button } from 'antd';
+const {Option} = AutoComplete;
+
+
 
 class App extends Component {
   state = {
@@ -22,18 +28,28 @@ class App extends Component {
   }
 
   render() {
+    // const options = this.state.result.items.map(item => item.snippet.title);
     return (
       <div className="App">
+        <AutoComplete
+          className="search_form"
+          size={"large"}
+          onChange={(value) => this.setState({ searchQuery: value })} 
+          value={this.state.searchQuery}
+        >
+          { this.state.result.items.map((item, index)  => <Option key={ index } value={item.snippet.title}>{ item.snippet.title }</Option> ) }
+        </AutoComplete>
+        <Button size={"large"}><SearchOutlined /></Button>
+         
         {this.state.loading && (
           <div>Loading ... </div>
         )}
-        <input onChange={(ev) => this.setState({ searchQuery: ev.target.value })} value={this.state.searchQuery} />
-        {this.state.searchQuery}
+
         {this.state.result.items.map(item => (
           <a key={item.id.videoId} href={`https://www.youtube.com/watch?v=${item.id.videoId}`} target="_blank">
             <div>
               <div>{item.snippet.title}</div>
-              <img src={item.snippet.thumbnails.high.url} />                
+              <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title}/>                
             </div>
           </a>
         ))}
